169705c38d6ffa5828da0915bd2376bf29464d67
medo123 <mrz.m7md@gmail.com>
2024-06-26 07:54:43 +0300

    finish prepare app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:test_corner_products/block_observer.dart';
+import 'package:test_corner_products/core/services/services_locator.dart';
 import 'package:test_corner_products/corner_app.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // ensure that everything in this function already finished then start application `runApp()`
+
+  usePathUrlStrategy(); // remove # from links
+
+  Bloc.observer = MyBlocObserver();
+
+  ServicesLocator().init();
+
   runApp(const MyApp());
 }
 
